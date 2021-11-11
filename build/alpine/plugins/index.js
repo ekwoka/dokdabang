ee98eed916608537261319ab7ca4898a3b15c237
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-11-12 01:18:31 +0800

    :arrow_up:

--- build/alpine/plugins/index.js
+++ build/alpine/plugins/index.js
@@ -1,4 +1,4 @@
-import persistedStore from './persistedStore'
+import persistedStore from '@ekwoka/persistedstore'
 import RIAS from '@ekwoka/x-rias'
 import intersect from '@alpinejs/intersect'
 import collapse from '@alpinejs/collapse'
