b18fdf3427db2c4e83b356259d3691aa8948eeea
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-11-14 19:42:52 +0800

    :arrow_up: X-RIAS

--- build/alpine/plugins/index.js
+++ build/alpine/plugins/index.js
@@ -3,4 +3,4 @@ import RIAS from '@ekwoka/x-rias'
 import intersect from '@alpinejs/intersect'
 import collapse from '@alpinejs/collapse'
 
-export default Plugins = [persistedStore,intersect,collapse]
+export default Plugins = [persistedStore,intersect,collapse,RIAS('dmaoqyvwt')]
