682ac01ae48b2429ac945673a6b3b296e9049825
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-11-14 19:51:53 +0800

    :bug: RIAS testing

--- build/alpine/plugins/index.js
+++ build/alpine/plugins/index.js
@@ -3,4 +3,9 @@ import RIAS from '@ekwoka/x-rias'
 import intersect from '@alpinejs/intersect'
 import collapse from '@alpinejs/collapse'
 
-export default Plugins = [persistedStore,intersect,collapse,RIAS('dmaoqyvwt')]
+const RIASconfig = {
+    key: 'dmaoqyvwt',
+    autoSize: true
+}
+
+export default Plugins = [persistedStore,intersect,collapse,RIAS(RIASconfig)]
