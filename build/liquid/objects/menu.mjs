0d8f76bb318af23755a0edb295c16a79d7fe915d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-11-27 16:19:16 -0800

    :lipstick: Added more Menu Items

--- build/liquid/objects/menu.mjs
+++ build/liquid/objects/menu.mjs
@@ -18,7 +18,7 @@ export default [
         title: 'Cafe Latte',
         handle: 'cafe-latter',
         description: 'A latte made with coffee',
-        price: 3200,
+        price: 6200,
         reviews: {
             count: 1278,
             average: 4.4
@@ -28,5 +28,80 @@ export default [
             'hot'
         ],
         image: 'https://placekitten.com/1010/1010'
+    },
+    {
+        title: 'Cafe Espresso',
+        handle: 'espresso',
+        description: 'Straight up espresso',
+        price: 5200,
+        reviews: {
+            count: 878,
+            average: 4.2
+        },
+        variants: [
+            'iced',
+            'hot'
+        ],
+        image: 'https://placekitten.com/1020/1020'
+    },
+    {
+        title: 'Cafe Americano',
+        handle: 'americano',
+        description: 'Dark coffee like your grandpa drinks',
+        price: 4200,
+        reviews: {
+            count: 778,
+            average: 4.0
+        },
+        variants: [
+            'iced',
+            'hot'
+        ],
+        image: 'https://placekitten.com/1030/1030'
+    },
+    {
+        title: 'Cappuccino',
+        handle: 'cappuccino',
+        description: 'A cappuccino made with milk and coffee',
+        price: 3200,
+        reviews: {
+            count: 678,
+            average: 3.8
+        },
+        variants: [
+            'iced',
+            'hot'
+        ],
+        image: 'https://placekitten.com/1040/1040'
+    },
+    {
+        title: 'Mocha Latte',
+        handle: 'mocha-latte',
+        description: 'A latte made with chocolate',
+        price: 7000,
+        reviews: {
+            count: 878,
+            average: 4.2
+        },
+        variants: [
+            'iced',
+            'hot'
+        ],
+        image: 'https://placekitten.com/1050/1050'
+    },
+    {
+        title: 'Caramel Macchiato',
+        handle: 'caramel-macchiato',
+        description: 'A macchiato made with caramel',
+        price: 7000,
+        reviews: {
+            count: 778,
+            average: 4.0
+        },
+        variants: [
+            'iced',
+            'hot'
+        ],
+        image: 'https://placekitten.com/1060/1060'
     }
 ]
