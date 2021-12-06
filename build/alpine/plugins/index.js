6ffe2260c0135bc4ec8e44fe7183c1b0951fb946
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-12-06 13:49:32 -0800

    :sparkles: X-Trap

--- build/alpine/plugins/index.js
+++ build/alpine/plugins/index.js
@@ -2,10 +2,11 @@ import persistedStore from '@ekwoka/persistedstore'
 import RIAS from '@ekwoka/x-rias'
 import intersect from '@alpinejs/intersect'
 import collapse from '@alpinejs/collapse'
+import trap from '@alpinejs/trap/src'
 
 const RIASconfig = {
     key: 'dmaoqyvwt',
     autoSize: true
 }
 
-export default Plugins = [persistedStore,intersect,collapse,RIAS(RIASconfig)]
+export default Plugins = [persistedStore, RIAS(RIASconfig), trap]
